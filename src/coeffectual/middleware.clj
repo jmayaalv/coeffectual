1acd3d0e85481ab6f5005663d635f83e321e22f8
Juan Maya <juan.maya@kane-group.com>
2025-05-29 09:24:42 +0200

    support for var

--- src/coeffectual/middleware.clj
+++ src/coeffectual/middleware.clj
@@ -1,6 +1,13 @@
 (ns coeffectual.middleware)
 
+(defn execute-handler
+  [context handler]
+  (if (var? handler)
+    ((deref handler) context)
+    (handler context)))
+
 (defn wrap-coeffect
   [handler id coeffect-fn]
   (fn [context]
-    (handler (assoc context id (coeffect-fn context)))))
+    (execute-handler (assoc context id (coeffect-fn context))
+                     handler)))
